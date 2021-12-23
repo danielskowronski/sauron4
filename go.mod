0d4c8c0d088f6f0c99ee68c0e1102dcfda1dda91
Daniel Skowroński <daniel@dsinf.net>
2021-12-23 16:58:48 +0100

    add simple data sending to InfluxDB

--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.17
 
 require (
 	github.com/go-ping/ping v0.0.0-20211130115550-779d1e919534
+	github.com/influxdata/influxdb1-client v0.0.0-20200827194710-b269163b24ab
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/google/uuid v1.2.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- sauron4.go
+++ sauron4.go
@@ -3,21 +3,24 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"time"
 
 	"github.com/go-ping/ping"
+	client "github.com/influxdata/influxdb1-client"
 	"gopkg.in/yaml.v2"
 )
 
 // Config is struct of config file
 type Config struct {
 	InfluxDB struct {
-		Host string `yaml:"host"`
-		Port string `yaml:"port"`
-		DB   string `yaml:"db"`
-		User string `yaml:"user"`
-		Pass string `yaml:"pass"`
+		Enabled bool   `yaml:"enabled"`
+		Host    string `yaml:"host"`
+		Port    int    `yaml:"port"`
+		DB      string `yaml:"db"`
+		User    string `yaml:"user"`
+		Pass    string `yaml:"pass"`
 	} `yaml:"influxdb"`
 	WhoAmI       string `yaml:"whoami"`
 	PingerParams struct {
@@ -60,5 +63,43 @@ func main() {
 		stats := pinger.Statistics()
 
 		fmt.Printf("%s_%s: loss %.2f%%, rtt %dms\n", config.WhoAmI, target.Name, stats.PacketLoss, stats.AvgRtt.Milliseconds())
+
+		if config.InfluxDB.Enabled {
+			host, hostParseErr := url.Parse(fmt.Sprintf("http://%s:%d", config.InfluxDB.Host, config.InfluxDB.Port))
+			if hostParseErr != nil {
+				fmt.Println(hostParseErr)
+				continue
+			}
+			c, connErr := client.NewClient(client.Config{
+				URL:      *host,
+				Username: config.InfluxDB.User,
+				Password: config.InfluxDB.Pass,
+			})
+			if connErr != nil {
+				fmt.Println("Error creating InfluxDB Client: ", connErr.Error())
+				continue
+			}
+
+			pts := make([]client.Point, 1)
+			pts[0] = client.Point{
+				Measurement: fmt.Sprintf("%s_%s", config.WhoAmI, target.Name),
+				Fields: map[string]interface{}{
+					"rtt":  stats.AvgRtt.Milliseconds(),
+					"loss": stats.PacketLoss,
+				},
+				Time:      time.Now(),
+				Precision: "ns",
+			}
+
+			bps := client.BatchPoints{
+				Points:   pts,
+				Database: config.InfluxDB.DB,
+			}
+			_, writeErr := c.Write(bps)
+			if writeErr != nil {
+				fmt.Println(writeErr)
+				continue
+			}
+		}
 	}
 }
